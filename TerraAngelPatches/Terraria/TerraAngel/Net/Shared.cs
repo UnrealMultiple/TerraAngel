6fef5cd58828222baa0aa6fec6ce3c16f8555013
Cai <13110818005@qq.com>
2026-02-24 18:16:28 +0800

    fix: upgrade Net Debugger for Terraria 1.4.5

--- TerraAngelPatches/Terraria/TerraAngel/Net/Shared.cs
+++ TerraAngelPatches/Terraria/TerraAngel/Net/Shared.cs
@@ -1,12 +1,15 @@
-using TrProtocol;
+extern alias TrProtocol;
+using TrProtocol::TrProtocol;
 
 namespace TerraAngel.Net;
+
+extern alias TrProtocol;
 
 public static class Shared
 {
     // serialize s->c, deserialize c->s
-    public static readonly PacketSerializer ServerPacketSerializer = new(false, $"Terraria{Main.curRelease}");
+    public static readonly PacketSerializer ServerPacketSerializer = new(false, (string)$"Terraria{Main.curRelease}");
     
     // serialize c->s, deserialize s->c
-    public static readonly PacketSerializer ClientPacketSerializer = new(true, $"Terraria{Main.curRelease}");
+    public static readonly PacketSerializer ClientPacketSerializer = new(true, (string)$"Terraria{Main.curRelease}");
 }
