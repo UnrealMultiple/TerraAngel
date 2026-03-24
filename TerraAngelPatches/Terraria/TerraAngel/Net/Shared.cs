f3fbf069929d31de151d450abfa6bf658f3900fe
LaoSparrow <laosparrow@outlook.com>
2026-03-25 00:10:19 +0800

    deps: bump TrProtocol

--- TerraAngelPatches/Terraria/TerraAngel/Net/Shared.cs
+++ TerraAngelPatches/Terraria/TerraAngel/Net/Shared.cs
@@ -6,8 +6,8 @@ namespace TerraAngel.Net;
 public static class Shared
 {
     // serialize s->c, deserialize c->s
-    public static readonly PacketSerializer ServerPacketSerializer = new(false, $"Terraria{Main.curRelease}");
+    public static readonly PacketSerializer ServerPacketSerializer = new(false);
     
     // serialize c->s, deserialize s->c
-    public static readonly PacketSerializer ClientPacketSerializer = new(true, $"Terraria{Main.curRelease}");
+    public static readonly PacketSerializer ClientPacketSerializer = new(true);
 }
