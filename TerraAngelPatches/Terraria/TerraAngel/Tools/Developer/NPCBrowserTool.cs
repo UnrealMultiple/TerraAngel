20eb97b18148dd6a473a156e04809c025b01b5d3
Controllerdestiny <523321293@qq.com>
2026-03-24 23:15:27 +0800

    feat: lightning effects and boss spawning

--- TerraAngelPatches/Terraria/TerraAngel/Tools/Developer/NPCBrowserTool.cs
+++ TerraAngelPatches/Terraria/TerraAngel/Tools/Developer/NPCBrowserTool.cs
@@ -8,10 +8,14 @@ public class NPCBrowserTool : Tool
 
     public override ToolTabs Tab => ToolTabs.NewTab;
 
+    private static readonly int[] BossID = [4, 13, 50, 126, 125, 134, 127, 128, 131, 129, 130, 222, 245, 266, 370, 657, 668, 398];
+
     public string NPCSearch = "";
 
     public override void DrawUI(ImGuiIOPtr io)
     {
+        if (ImGui.Button(GetString("Spawn All Boss")))
+            SpawnAllBoss();
         ImGui.TextUnformatted(GetString("Search")); 
         ImGui.SameLine();
 
@@ -63,6 +67,31 @@ public class NPCBrowserTool : Tool
         {
             var coords = Main.LocalPlayer.Center.ToPoint();
             NPC.NewNPC(new EntitySource_SpawnNPC(), coords.X, coords.Y, npcID);
+        }
+    }
+
+    public static void SpawnAllBoss()
+    {
+        foreach (var id in BossID)
+        {
+            SpawnBoss(id);
+        }
+    }
+
+    public static void SpawnBoss(int npcID)
+    {
+        switch (Main.netMode)
+        {
+            case 0:
+                NPC.SpawnOnPlayer(Main.LocalPlayer.whoAmI, npcID);
+                break;
+            case 1:
+                PacketBuilder.FastSendPacket(MessageID.SpawnBossUseLicenseStartEvent, b =>
+                {
+                    b.Write((short)Main.myPlayer);
+                    b.Write((short)npcID);
+                });
+                break;
         }
     }
 }
---
+++ TerraAngelPatches/Terraria/TerraAngel/Tools/Visuals/LightningTool.cs
@@ -0,0 +1,49 @@
+﻿using Terraria.GameContent.Drawing;
+
+namespace TerraAngel.Tools.Visuals;
+
+public class LightningTool : Tool
+{
+    public override string Name => GetString("Lightning Tool");
+
+    public override ToolTabs Tab => ToolTabs.VisualTools;
+
+    private bool ShowLightning = false;
+    private int RandomFrame = 200;
+    private int LightningCount = 7;
+    private int _FrameCounter = 0;
+
+    public override void DrawUI(ImGuiIOPtr io)
+    {
+        ImGui.Checkbox(GetString("Lightning Effect"), ref ShowLightning);
+        if(ShowLightning && ImGui.CollapsingHeader(GetString("Lightning Settings")))
+        {
+            ImGui.SliderInt(GetString("Generate frame"), ref RandomFrame, 1, 300);
+            ImGui.SliderInt(GetString("Number of lightning"), ref LightningCount, 1, 300);
+        }
+    }
+
+    public override void Update()
+    {
+        if(ShowLightning)
+        {
+            _FrameCounter++;
+            if(_FrameCounter % RandomFrame == 0)
+            {
+                for (var i = 0; i < LightningCount; i++)
+                {
+                    var randomColor = new Color(Main.rand.Next(0, 255), Main.rand.Next(0, 255), Main.rand.Next(0, 255));
+                    var direction = Main.rand.Next(0, 2) == 0 ? -1 : 1;
+                    var settings = new ParticleOrchestraSettings()
+                    {
+                        PositionInWorld = Main.LocalPlayer.position + new Vector2(Main.rand.Next(60 * 16) * direction, 0),
+                        UniqueInfoPiece = (int)randomColor.PackedValue,
+                        MovementVector = new Vector2(Main.rand.Next(0, 1145), 0f),
+                        IndexOfPlayerWhoInvokedThis = (byte)Main.myPlayer
+                    };
+                    ParticleOrchestrator.BroadcastOrRequestParticleSpawn(ParticleOrchestraType.StormLightning, settings);
+                }
+            }
+        }
+    }
+}
